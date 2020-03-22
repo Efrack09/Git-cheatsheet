00286a3e9414bbc06d6157a34a6f05b74067b09f
Efrain Matu <59808231+Efrack09@users.noreply.github.com>
2020-03-22 15:17:57 -0600

    Update EXE11.c

--- Exercises_quarantine/EXE11.c
+++ Exercises_quarantine/EXE11.c
@@ -24,32 +24,30 @@ int main(int argc, char * argv[]) {
 }
 
 /*
-CODE
 int main(int argc, char * argv[]) {
-    int i = 0;//is important create the index for the future loops
+    int i = 0;
     int j = 0;
-    char * states[] = {//we going to create a array of strings with some state names
+    char * states[] = {
         "California",
         "Oregon",
         "Washington",
         "Texas"
     };
-    while (i < argc) {//this is a while-loop and count all arguments with argc and print each one with argv
+    while (i < argc) {
         printf("arg %d: %s\n", i, argv[i]);
         states[j+4] = argv[j];
         j++;
         i++;
     }
-    int num_states = 4+argc;//create an axuliar variable to count
-    i = 0;//restart the index
-    while (i < num_states) {//here we will print all states
+    int num_states = 4+argc;
+    i = 0;
+    while (i < num_states) {
         printf("state %d: %s\n", i, states[i]);
         i++;
     }
     return 0;
 }
-OUT
-./a.out test it l
+Result 1
 arg 0: ./a.out
 arg 1: test
 arg 2: it
@@ -62,4 +60,39 @@ state 4: ./a.out
 state 5: test
 state 6: it
 state 7: l
+
+
+Result 2
+arg 0: ./a.out
+arg 1: test
+arg 2: it
+arg 3: l
+state 0: California
+state 1: Oregon
+state 2: Washington
+state 3: Texas
+state 4: ./a.out
+state 5: test
+state 6: it
+state 7: l
+
+Result 3
+
+arg 1: test
+arg 2: it
+arg 3: l
+arg 4: r
+arg 5: t
+arg 6: s
+state 0: California
+state 1: Oregon
+state 2: Washington
+state 3: Texas
+state 4: ./a.out
+state 5: test
+state 6: it
+state 7: l
+state 8: r
+state 9: t
+state 10: s
 */
