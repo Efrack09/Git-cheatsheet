82d0f2339e7d009c93c617e1debf5613fd9e950d
Efrain Matu <59808231+Efrack09@users.noreply.github.com>
2020-03-22 15:14:11 -0600

    Update EXE10.c

--- Exercises_quarantine/EXE10.c
+++ Exercises_quarantine/EXE10.c
@@ -22,4 +22,40 @@ state 0: California
 state 1: (null)
 state 2: Washington
 state 3: Texas
+
+
+Result 2
+#include <stdio.h>
+int main(int argc, char * argv[]) {
+    int i = 0;
+    for (i = 1; i < argc; i++) {
+        printf("arg %d: %s\n", i, argv[i]);
+    }
+    char * states[] = {
+        NULL,
+        "Oregon",
+        "Washington",
+        "Texas"
+    };
+    int num_states = 4;
+    for (i = 0; i < num_states; i++) {
+        printf("state %d: %s\n", i, states[i]);
+    }
+    return 0;
+}
+./a.out Merida
+arg 1: Merida
+state 0: (null)
+state 1: Oregon
+state 2: Washington
+state 3: Texas
+
+Result 3
+
+./a.out Merida
+arg 1: Oregon
+state 0: Merida
+state 1: Oregon
+state 2: Washington
+state 3: Texas
 */
